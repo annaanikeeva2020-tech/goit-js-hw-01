143c50c5a6ab63515c8d0590b10acb2190ed06cc
haaniki <annaanikeeva2020@gmail.com>
2026-03-10 20:01:13 +0100

    add homework js

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ function makeTransaction(quantity, pricePerDroid) {
 
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    
+
 }
 
 
